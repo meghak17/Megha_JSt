1662702b3610ce46220933be22c0f823c1c322cf
Megha Kavathekar <158732482+meghak17@users.noreply.github.com>
2024-06-03 11:53:45 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//to compare two datatypes should be same
+// console.log("2">1);//true
+// console.log("02">1);//true
+
+// console.log(null>0);//false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true
+//== and >< worls differently
+// console.log(undefined == 0);//false
+// console.log(undefined >= 0);//false
+
+//strict check data types too checked
+console.log("0" === 0); //== => true  === => false
+
+
+
