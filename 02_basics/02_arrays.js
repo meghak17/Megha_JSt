c66d883fbc71ec1c1f4f67b386d736c1d8fa7c63
Megha Kavathekar <158732482+meghak17@users.noreply.github.com>
2024-06-03 11:57:39 +0000

    array

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,25 @@
+const marvel_heroes = ["thor","Ironman","spiderman"]
+const dc_heroes = ["superman","flash","batman"]
+
+//marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[1]);
+
+//  const all = marvel_heroes.concat(dc_heroes)
+//  console.log(all);
+
+//const all_hero = [...marvel_heroes,...dc_heroes]//spread
+//console.log(all_hero);
+
+// const arr = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real = arr.flat(7)
+// console.log(real);
+
+// console.log(Array.isArray("Megha"));//false
+// console.log(Array.from("Megha"));//[ 'M', 'e', 'g', 'h', 'a' ]
+// console.log(Array.from({name:"Megha"}));//[]...interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));//makes new array
