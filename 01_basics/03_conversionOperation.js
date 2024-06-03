3edaecb49d036011153009b408cc4e45207b64ed
Megha Kavathekar <158732482+meghak17@users.noreply.github.com>
2024-06-03 11:53:33 +0000

    operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,58 @@
+let score = "33"
+// console.log(typeof score)//string
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);//number
+
+let score1 = "33aa"
+// console.log(typeof score1)//string
+let valueInNumber1 = Number(score1)
+// console.log(typeof valueInNumber1);//number
+// console.log(valueInNumber1);//NaN
+
+//"33"=> 33
+//"33aa"=>NaN but type is number
+// true => 1 false => 0
+
+let isLoggedIn = 1
+
+let bool = Boolean(isLoggedIn)
+// console.log(bool);//true
+
+//1 => true, 0 => false
+//Megha => true ,"" => false
+
+// conversion to string
+ let some = 33
+ let str = String(some)
+//  console.log(str);
+//  console.log(typeof str);
+
+ //***********************operatore***********************//
+
+ let value = 3
+ let negValue = -value
+//  console.log(negValue); -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 ="hello "
+let str2 = "megha"
+// console.log(str1+str2);//hello megha
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log(1+2+"2");//32
+console.log("1"+2+2);//122
+// true => true
+//+true => 1
+//true+ => error
+//+"" => 0
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+let count = 100
+count++//++count=> 101
+console.log(count);
