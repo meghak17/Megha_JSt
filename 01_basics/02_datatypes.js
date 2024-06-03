e0d06ad567ebca1fbdb1ade16314062e89a0d0fd
Megha Kavathekar <158732482+meghak17@users.noreply.github.com>
2024-06-03 11:53:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+2)//we are using nodejs,not browser
+
+// console.log(3+3) console.log("megha") ....error not a good practice
+let name ="megha" 
+let age= 18
+let isLoggedIn = false
+let state //undefined
+let value =null //null
+console.log(state);
+console.log(value);
+//primitives------
+//number => 2 to power 3
+//bigint => 123455677978889
+//string => ""
+//boolean => true/false
+//null => standalone value = empty value 
+//undefined => value is not assigned
+//symbol => unique
+
+//nonprimitive----
+//object--
+//type of null => object
+//type of undefined => undefined
